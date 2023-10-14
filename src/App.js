4ba9f4fbba9015fd7093f56832b8d6eacbfb7253
poojit30 <poojitsai.d2021@vitstudent.ac.in>
2023-10-14 21:48:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Paragraph } from './components/paragraph';
+
+function App() {
+  return (
+    <div>
+      <h2 style={{color:"Black"}}>React-Task-1</h2>
+      <Paragraph />
+    </div>
+  );
+}
+
+export default App;
